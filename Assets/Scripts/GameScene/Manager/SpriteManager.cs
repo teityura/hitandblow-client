dc7f28f3e8550df6120df43faf9377fc1c4917fe
teityura <teityura@gmail.com>
2021-10-10 03:54:12 +0900

    ヒットかブローしたときの画像と変数を追加

--- Assets/Scripts/GameScene/Manager/SpriteManager.cs
+++ Assets/Scripts/GameScene/Manager/SpriteManager.cs
@@ -4,27 +4,19 @@ namespace HitBlow.Manager
 {
     public class SpriteManager : MonoBehaviour
     {
-        private static SpriteManager instance;
-        public static SpriteManager I
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new SpriteManager();
-                }
-                return instance;
-            }
-        }
-
         [SerializeField]
         private Sprite[] numberSpritesSerialized = null;
 
+        [SerializeField]
+        private Sprite[] hitNumberSpritesSerialized = null;
+
         private static Sprite[] numberSprites = null;
+        private static Sprite[] hitNumberSprites = null;
 
         private void Awake()
         {
             numberSprites = numberSpritesSerialized;
+            hitNumberSprites = hitNumberSpritesSerialized;
         }
 
         public static Sprite GetNumberSprite(int number)
