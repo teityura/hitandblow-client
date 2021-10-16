2e15e7a4d3f5973ae2b31ae23e2654f8f1fed23e
teityura <teityura@gmail.com>
2021-10-17 01:40:04 +0900

    GameStartButton.cs を追加

---
+++ Assets/Scripts/TitleScene/GameStartButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HitBlow.MyButton
+{
+    public class GameStartButton : MonoBehaviour
+    {
+        private ActionButton standardButton = null;
+
+        private void Awake()
+        {
+            standardButton = GetComponent<ActionButton>();
+        }
+
+        private void Start()
+        {
+            standardButton.OnClick(() => SceneManager.LoadScene("GameScene"));
+        }
+    }
+}
