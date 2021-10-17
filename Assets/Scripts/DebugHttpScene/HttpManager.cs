b697e3c7544e22aa1a2ee71c8c799fb52545a6bb
teityura <teityura@gmail.com>
2021-10-17 23:35:06 +0900

    DebugHttpSceneシーンを作成

---
+++ Assets/Scripts/DebugHttpScene/HttpManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System;
+
+public static class HttpManager
+{
+    public const string SERVER_URL = "http://34.145.109.41:5000";
+    public static Action<string> onComplete;
+
+    public static async void SendRequest(string path)
+    {
+        HttpClient client = new HttpClient();
+        var responce = await client.GetStringAsync($"{SERVER_URL}/{path}");
+        onComplete?.Invoke(responce);
+    }
+
+    public static void OnComplete(Action<string> callback)
+    {
+        onComplete = callback;
+    }
+}
