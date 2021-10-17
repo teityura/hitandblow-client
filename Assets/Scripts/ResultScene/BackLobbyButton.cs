770b955aa23ed68258082260ecadab2d28235781
teityura <teityura@gmail.com>
2021-10-17 17:30:30 +0900

    BackLobbyButtonでロビーに戻れるように

---
+++ Assets/Scripts/ResultScene/BackLobbyButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using HitBlow.Manager;
+
+namespace HitBlow.MyButton
+{
+    public class BackLobbyButton : MonoBehaviour
+    {
+        private ActionButton standardButton = null;
+
+        private void Awake()
+        {
+            standardButton = GetComponent<ActionButton>();
+        }
+
+        private void Start()
+        {
+            standardButton.OnClick(() =>ToLobbyScene());
+        }
+
+        private void ToLobbyScene()
+        {
+            SceneManager.LoadScene("LobbyScene");
+            GameManager.SetGamePhase(GameManager.GAME_PHASE.LOBBY_START);
+        }
+    }
+}
